1f6a66d38354e6c0c058920073f49d379a925d01
shurijoc <re.e.poch89@gmail.com>
2023-03-16 19:09:44 +0900

    :+1: fix drop

--- src/database/migrations/2023_03_16_040737_add_address_to_users.php
+++ src/database/migrations/2023_03_16_040737_add_address_to_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('address');
+            $table->string('address')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('address');
+            $table->dropColumn('address');
         });
     }
 };
